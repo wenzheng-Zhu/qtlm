76ee8c9c02c7f281f9f7a23870156ce7801677d8
rippletech <rippletech@rippletechs-MacBook-Pro.local>
2018-07-25 11:25:28 +0800

    添加gem

---
+++ config/deploy/production.rb
@@ -0,0 +1,21 @@
+
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
+
+
+
+server "47.92.1.46", user: "deploy", roles: %w{app db web}, my_property: :my_value
+
+set :ssh_options, {
+  keys: %w(~/.ssh/id_rsa),
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
