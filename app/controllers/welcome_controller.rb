e3fbffae960d62a70a382c9fae617c570be198f5
rippletech <rippletech@rippletechs-MacBook-Pro.local>
2018-07-27 02:10:44 +0800

    修改7

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class WelcomeController < ApplicationController
 
-    skip_before_action :verify_authenticity_token, only: [:fuck] 
+    skip_before_action :verify_authenticity_token, only: [:make] 
 
     def make
 
@@ -108,16 +108,7 @@ class WelcomeController < ApplicationController
     end
 
 
-    def fuck
 
-        uri4 = URI("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx7eb44ce11b9ce817&secret=fd4e5dc0c362526f12371ab0bb2255d1")
-        http4 = Net::HTTP.new(uri4.host, uri4.port)
-        http4.use_ssl = true
-        header = {'content-type'=>'application/json'}
-        @test = http4.get(uri4, header).body.split("\"")[3]
-        return render json: {}, status: 200
-        
-    end
     
 end
 
