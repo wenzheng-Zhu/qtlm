d155ca27b61da1cb8230e756bd2ca26e9e8c79d0
wenzheng-Zhu <zwz_322@126.com>
2018-08-01 13:48:48 +0800

    修改36

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
   #post 'callback' => 'welcome#make'
   post 'callback' => 'welcome#shixian'
-  post 'wxcallback' => 'welcome#huiyuanka'
+  post 'wx' => 'welcome#huiyuanka'
   # get '/foo' => 'welcome#bar'
   root 'home#index'
 end
