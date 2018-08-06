0e57b1725c50e57079d1c78bdee22a77bd42f412
wenzheng-Zhu <zwz_322@126.com>
2018-08-06 11:42:45 +0800

    修改49增加会员读取已买课程链接

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class WelcomeController < ApplicationController
 
-    skip_before_action :verify_authenticity_token, only: [:shixian] 
+    skip_before_action :verify_authenticity_token, only: [:shixian, :bar] 
 
  def shixian
 
